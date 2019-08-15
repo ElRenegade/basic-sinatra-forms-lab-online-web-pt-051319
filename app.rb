50ce05297d0940c3af45e8f523e86328ccc17f45
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-15 03:19:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,18 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/newteam' do
-    erb :newteam
-  end
+     erb :newteam
+   end
 
-  post '/team' do
-    @name = params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @center = params["c"]
+   post '/team' do
+     @name = params["name"]
+     @coach = params["coach"]
+     @pg = params["pg"]
+     @sg = params["sg"]
+     @sf = params["sf"]
+     @pf = params["pf"]
+     @center = params["c"]
 
-    erb :team
-  end
+     erb :team
+   end
 end
